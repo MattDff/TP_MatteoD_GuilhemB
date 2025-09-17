4f258bff1aac80b4c1bfc654d0a1347f9e2efa89
MatteoEnsea <matt.doffe@gmail.com>
2025-09-17 11:59:47 +0200

    avancé

--- TP1/ex2.c
+++ TP1/ex2.c
@@ -8,11 +8,19 @@
 
 int main(){
     struct stat sb;
+    //char* buffer[];
 
     int file = open("texte.txt", O_RDWR);
     stat("texte.txt",&sb);
     long size_file = sb.st_size;
     printf("Taille du fichier : %ld octets\n",size_file);
+
+    /*
+    for (int octet = 0; octet<size_file;octet += BUFFER_SIZE){
+        mmap();
+    }
+    */
+        
 
     return 0;
 }
