b8755a82f5dee38a5eb2d54e031849d437e40b05
MatteoEnsea <matt.doffe@gmail.com>
2025-09-24 11:07:11 +0200

    Update

--- TP2/alloc_sec.c
+++ TP2/alloc_sec.c
@@ -1,7 +1,20 @@
 #include <alloc_sec.h>;
 
+HEADER * free_list = NULL;
+
+
+
 void * malloc_3is(size_t size){
-    
+
+    HEADER *currentent = free_list;
+
+    while (currentent != NULL) {
+        if (currentent->bloc_size < size){
+            
+        }
+        currentent = currentent->ptr_next;
+    }
+
     return;
 }
 
--- TP2/alloc_sec.h
+++ TP2/alloc_sec.h
@@ -3,6 +3,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define MAGIC_NUMBER 0x0123456789ABCDEFL
+
+typedef struct HEADER_TAG {
+    struct HEADER_TAG * ptr_next; /* pointe sur le prochain bloc libre */
+    size_t bloc_size; /* taille du memory bloc en octets*/
+    long magic_number; /* 0x0123456789ABCDEFL */
+} HEADER;
+
 void * malloc_3is(size_t size);
 
 void free_3is(void * pointer);
