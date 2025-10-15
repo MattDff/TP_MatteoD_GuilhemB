91386faaba70f23d78d3356fa0c3fd613bbeb645
MatteoEnsea <matt.doffe@gmail.com>
2025-10-15 15:50:30 +0200

    start

--- TP4/client.c
+++ TP4/client.c
@@ -1,5 +1,34 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+
 #include "segdef.h"
 
-int main(){
+struct sembuf sop;
+
+void init(int shmid, int semid, segment* seg){
+    
+
+    if ((shmid=shmget(cle,segsize,0))){
+        perror("shmget");
+    }
+
+    if ((semid=semget(cle,3,0))){
+        perror("semget");
+    }
+
+    init_rand();
+}
+
+int main(int argc, char *argv[]){
+    int shmid, semid;
+    segment* seg;
+    struct shmseg seg;
+    init(shmid,semid,seg);
     return 0;
 }
