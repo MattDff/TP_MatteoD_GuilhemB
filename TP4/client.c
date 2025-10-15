8c4f3508ea9ce64ef6be29d266f81c30a9f98a33
MatteoEnsea <matt.doffe@gmail.com>
2025-10-15 16:15:40 +0200

    Add segment handle erro

--- TP4/client.c
+++ TP4/client.c
@@ -1,23 +1,27 @@
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/msg.h>
 
 #include "segdef.h"
 
 struct sembuf sop;
 
 void init(int shmid, int semid, segment* seg){
-    if ((shmid=shmget(cle,segsize,0))){
+    if ((shmid=shmget(cle,segsize,0)) == -1){
         perror("shmget");
+        exit(EXIT_FAILURE);
     }
 
-    if ((semid=semget(cle,3,0))){
+    if ((semid=semget(cle,3,0)) == -1){
         perror("semget");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((seg = (segment *) shmat(shmid, NULL, 0)) ==  -1) {
+        perror("shmat");
+        exit(EXIT_FAILURE);
     }
 
     init_rand();
