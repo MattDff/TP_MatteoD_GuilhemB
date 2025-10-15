b4329c590e5e183322fdbe5f0f06e801e6cc827b
MatteoEnsea <matt.doffe@gmail.com>
2025-10-15 17:12:09 +0200

    Client finished

--- TP4/client.c
+++ TP4/client.c
@@ -6,30 +6,56 @@
 
 #include "segdef.h"
 
-struct sembuf sop;
+#define REQ_MAX 3
 
-void init(int shmid, int semid, segment* seg){
-    if ((shmid=shmget(cle,segsize,0)) == -1){
+void init(int* shmid, int* semid, segment** seg){
+    if ((*shmid=shmget(cle,segsize,0)) == -1){
         perror("shmget");
         exit(EXIT_FAILURE);
     }
-    printf("%d\n",shmid);
-    if ((semid=semget(cle,3,0)) == -1){
+    printf("%d\n",*shmid);
+
+    if ((*semid=semget(cle,3,0)) == -1){
         perror("semget");
         exit(EXIT_FAILURE);
     }
-    printf("%d\n",semid);
-    if ((seg = (segment *) shmat(shmid, NULL, 0)) ==  -1) {
+    printf("%d\n",*semid);
+
+    if ((*seg = (segment *) shmat(*shmid, NULL, 0)) ==  -1) {
         perror("shmat");
         exit(EXIT_FAILURE);
     }
-    printf("%d\n",seg);
+    printf("%p\n",*seg);
+
     init_rand();
 }
 
 int main(int argc, char *argv[]){
     int shmid, semid;
     segment* seg;
-    init(shmid,semid,seg);
+
+    init(&shmid,&semid,&seg);
+    int cmpt = 0;
+    while(cmpt<REQ_MAX){
+        acq_sem(semid,seg_dispo);
+        long sum = 0;
+        for (int j=0; j<maxval; j++){
+            seg->tab[j] = getrand()%100;
+            sum += seg->tab[j];
+        }
+        long local_avrg = sum/maxval;
+        acq_sem(semid,seg_init);
+        wait_sem(semid, res_ok);
+        long serv_avrg = seg->result;
+        lib_sem(semid,seg_init);
+        lib_sem(semid,seg_dispo);
+
+        printf("Client : %d\n",local_avrg);
+        printf("Servor : %d\n",serv_avrg);
+
+        cmpt++;
+    }
+
+    shmdt(seg);
     return 0;
 }
